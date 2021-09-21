c813d06e51acaedee0c3439eca7ebbc8b5e844cd
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-09-21 12:32:34 +0530

    added cleanup function in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,61 +1,84 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Search = () =>{
+const Search = () => {
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
-    const [term, setTearm] = useState('programming');
-    const [results, setResults] = useState([]);
+  console.log(results);
 
-    console.log(results);
+  const onSearchChange = (event) => {
+    setTerm(event.target.value);
+  };
 
-    const onSearchChange = (event)=>{
-        setTearm(event.target.value);
-    }
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
 
-    useEffect(() => {
-        const search = async () =>{
-            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
-                params : {
-                    action : 'query',
-                    list : 'search',
-                    origin : '*',
-                    format : 'json',
-                    srsearch : term
-                }
-            });
+      setResults(data.query.search);
+    };
 
-            setResults(data.query.search);
+    if (term && !results.length) {
+      search();
+    } else {
+      // Adding time interval before next search
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
         }
+      }, 300);
 
-        search();
-    }, [term])
+      // cancelling the previous time interval when useEffect function is called again
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [term]);
 
-    const renderedResults = results.map(result =>{
-        return(
-            <div key={result.pageid} className="item">
-                <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    {result.snippet}
-                </div>
-            </div>
-        );
-    });
-
-    return(
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input className="input" value={term} onChange={(event)=>{onSearchChange(event)}}></input>
-                </div>
-            </div>
-            <div className="ui celled list">
-                {renderedResults}
-            </div>
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
         </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
     );
-}
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            value={term}
+            onChange={(event) => {
+              onSearchChange(event);
+            }}
+          ></input>
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
 
 export default Search;
