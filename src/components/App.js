f341af6d060bf26e3ce42e5840a1d7c0f7249d96
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-09-21 12:35:08 +0530

    made changes to Search component and added Dropdown component

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,51 @@
-import React from 'react';
-import Accordion from './Accordion';
-import Search from './Search';
+import React, { useState } from "react";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
 
 const items = [
-    {
-        title : "What is React.js?",
-        content : "It is a front-end JavaScript library"
-    },
-    {
-        title : "Why do we use React?",
-        content : "It allows reusability of code"
-    },
-    {
-        title : "What is your name?",
-        content : "My name is Subhajyoti Naskar"
-    }
-]
+  {
+    title: "What is React.js?",
+    content: "It is a front-end JavaScript library",
+  },
+  {
+    title: "Why do we use React?",
+    content: "It allows reusability of code",
+  },
+  {
+    title: "What is your name?",
+    content: "My name is Subhajyoti Naskar",
+  },
+];
 
-const App = ()=>{
-    return(
-        <div>
-            {/* <Accordion items={items}/> */}
-            <Search />
-        </div>
-    );
-}
+const options = [
+  {
+    lable: "The color Red",
+    value: "red",
+  },
+  {
+    lable: "The color Green",
+    value: "green",
+  },
+  {
+    lable: "The color Blue",
+    value: "blue",
+  },
+];
+
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      {/* <Accordion items={items}/> */}
+      {/* <Search /> */}
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
+    </div>
+  );
+};
 
 export default App;
