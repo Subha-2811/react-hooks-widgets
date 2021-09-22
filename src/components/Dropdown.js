1c95eb78fc6d3903d730e7c83d68a1000531757b
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-09-22 11:29:01 +0530

    Added useEffect and useRef function

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react/cjs/react.development";
 
 const Dropdown = ({ selected, onSelectedChange, options }) => {
   const [open, setOpen] = useState(false);
+
+  const ref = useRef();
+
+  // Added a manual event handler for body inside useEffect
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // It checks if the event is been called by dropdown then and returns without implementing the code for addEventListener if true
+      if (ref.current.contains(event.target)) {
+        // console.log(event.target);
+        return;
+      }
+      // console.log("Body");
+      setOpen(false);
+    };
+    document.body.addEventListener("click", onBodyClick, { capture: true });
+
+    // Cleanup function
+    return () => {
+      // Removing the event listner from the body when the Component is Unmounted
+      document.body.removeEventListener("click", onBodyClick);
+    };
+  }, []);
+
   const renderedOptions = options.map((option) => {
     // If the option is already selected it wont show on the dropdown menu
     if (selected === option) return null;
     return (
       <div
-        onClick={() => onSelectedChange(option)}
+        onClick={() => {
+          // console.log("Item");
+          onSelectedChange(option);
+        }}
         key={option.value}
         className="item"
       >
@@ -17,11 +44,14 @@ const Dropdown = ({ selected, onSelectedChange, options }) => {
     );
   });
   return (
-    <div className="ui form">
+    <div ref={ref} className="ui form">
       <div className="field">
         <lable className="lable">Select a Color</lable>
         <div
-          onClick={() => setOpen(!open)}
+          onClick={() => {
+            // console.log("Dropdown");
+            setOpen(!open);
+          }}
           className={`ui selection dropdown ${open ? "visible active" : ""}`}
         >
           <i className="dropdown icon"></i>
