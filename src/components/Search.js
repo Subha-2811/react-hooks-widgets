bc239f41070c2490e78ff2b4b9bbb9c544f82fdf
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-09-23 14:45:47 +0530

    Added useState and useEffect for debounceTerm

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,13 +3,17 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
-  // console.log(results);
-
-  const onSearchChange = (event) => {
-    setTerm(event.target.value);
-  };
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -19,29 +23,53 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
-
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
+    if (debouncedTerm) {
       search();
-    } else {
-      // Adding time interval before next search
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 300);
-
-      // cancelling the previous time interval when useEffect function is called again
-      return () => {
-        clearTimeout(timeoutId);
-      };
     }
-  }, [term]);
+  }, [debouncedTerm]);
+
+  // console.log(results);
+
+  const onSearchChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+  //       params: {
+  //         action: "query",
+  //         list: "search",
+  //         origin: "*",
+  //         format: "json",
+  //         srsearch: term,
+  //       },
+  //     });
+
+  //     setResults(data.query.search);
+  //   };
+
+  //   if (term && !results.length) {
+  //     search();
+  //   } else {
+  //     // Adding time interval before next search
+  //     const timeoutId = setTimeout(() => {
+  //       if (term) {
+  //         search();
+  //       }
+  //     }, 300);
+
+  //     // cancelling the previous time interval when useEffect function is called again
+  //     return () => {
+  //       clearTimeout(timeoutId);
+  //     };
+  //   }
+  // }, [term]);
 
   const renderedResults = results.map((result) => {
     return (
